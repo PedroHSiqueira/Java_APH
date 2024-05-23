e434ba03560d71809c1e8b81b4ecc7f4373de699
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-05-23 14:21:08 -0300

    Oitava aula: Laços de repetição ( For ) em Java

---
+++ 8_Repeticoes_P3/src/main/java/com/mycompany/repeticoes/Repeticoes.java
@@ -0,0 +1,14 @@
+package com.mycompany.repeticoes;
+
+public class Repeticoes {
+
+    public static void main(String[] args) {
+        for(int contador = 0; contador <= 3; contador++){
+            System.out.println("Cambalhota: " + contador);
+        }
+        
+        for(int contadorInverso = 100; contadorInverso >= 0; contadorInverso -= 10){
+            System.out.println("Numero: " + contadorInverso);
+        }
+    }
+}
