54d3339b467b56cd22488cd0db2d5b83ac762903
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-05-28 19:58:01 -0300

    Terceira aula: Getter, Setter e contrutores

---
+++ 2 - Curso Java POO/3 - Modificadores/src/main/java/com/mycompany/modificadores/Caneta.java
@@ -0,0 +1,50 @@
+package com.mycompany.modificadores;
+
+public class Caneta {
+    private String modelo;
+    private float ponta;
+    private String cor;
+    private boolean tampada;
+    
+    //Construtor
+
+    public Caneta(String modelo, float ponta, String cor, boolean tampada) {
+        this.modelo = modelo;
+        this.ponta = ponta;
+        this.cor = cor;
+        this.tampada = tampada;
+    }
+    
+    
+    public String getModelo(){
+        return modelo;
+    }
+    
+    public void setModelo(String m){
+        this.modelo = m;
+    }
+    
+    public float getPonta(){
+        return ponta;
+    }
+    
+    public void setPonta(float p){
+        this.ponta = p;
+    }
+    
+    public void tampar(){
+        this.tampada = true;
+    }
+    
+    public void destampar(){
+        this.tampada = false;
+    }
+    
+    public void status(){
+        System.out.println("Sobre a caneta");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Está tamapda: " + this.tampada);
+    }
+}
---
+++ 2 - Curso Java POO/3 - Modificadores/src/main/java/com/mycompany/modificadores/Modificadores.java
@@ -0,0 +1,16 @@
+package com.mycompany.modificadores;
+
+public class Modificadores {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("Bic", "Amarela", 0.7f);
+
+//        c1.setModelo("Bic");
+//        // c1.modelo = "Bic"; ERROR
+//        
+//        c1.setPonta(0.5f);
+//        // c1.ponta = 0.5f; ERROR
+
+        c1.status();
+    }
+}
